251c327c2905818c6eb6d0692637be8f61f48753
sk <suryaveda@hotmail.com>
2019-05-24 11:02:28 +0100

    foreign commit

--- ARSU/ARSU/settings/prod.py
+++ ARSU/ARSU/settings/prod.py
@@ -1,11 +1,18 @@
 from ARSU.settings.base import *
 
-DEBUG = False
-ALLOWED_HOSTS = ['178.32.245.193']
+DEBUG = True
+ALLOWED_HOSTS = ['*']
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
 DATABASES = {
-            
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'aiims',
+        'USER': 'u_surya',
+        'PASSWORD': 'SU@@1997',
+        'HOST': 'localhost',
+        'PORT': '5432',
+    }        
 }
 try:
     from ARSU.settings.local import *
