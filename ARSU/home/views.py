85e7e3746f8508a0a5a61758cb3301a706e153ad
suryaveda <suryaaddu@gmail.com>
2019-05-26 04:04:47 +0530

    HH

--- ARSU/home/views.py
+++ ARSU/home/views.py
@@ -17,59 +17,15 @@ from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 def home(request):
     template_name = 'home/homePage.html'
     schedules = Schedule.objects.filter(batches = request.user.batch).order_by('pk')
-    page = request.GET.get('page', 1)
-    paginator = Paginator(schedules, 8)
-    try:
-        x = paginator.page(page)
-    except PageNotAnInteger:
-        x = paginator.page(1)
-    except EmptyPage:
-        x = paginator.page(paginator.num_pages)
-
     attendance = Attendance.objects.filter(batches = request.user.batch).order_by('pk')
-
-    paginator = Paginator(attendance, 2)
-    try:
-        y = paginator.page(page)
-    except PageNotAnInteger:
-        y = paginator.page(1)
-    except EmptyPage:
-        y = paginator.page(paginator.num_pages)
-
     results = Results.objects.filter(batches = request.user.batch).order_by('pk')
-
-    paginator = Paginator(results, 10)
-    try:
-        z = paginator.page(page)
-    except PageNotAnInteger:
-        z = paginator.page(1)
-    except EmptyPage:
-        z = paginator.page(paginator.num_pages)
-
-    NAposts = NonAcademic.objects.order_by('pk')
-
-    paginator = Paginator(NAposts, 2)
-    try:
-        a = paginator.page(page)
-    except PageNotAnInteger:
-        a = paginator.page(1)
-    except EmptyPage:
-        a = paginator.page(paginator.num_pages)
-
-    Cposts = CampusNews.objects.order_by('pk')
-
-    paginator = Paginator(Cposts, 10)
-    try:
-        b = paginator.page(page)
-    except PageNotAnInteger:
-        b = paginator.page(1)
-    except EmptyPage:
-        b = paginator.page(paginator.num_pages)
+    naca = NonAcademic.objects.order_by('pk')
+    camp = CampusNews.objects.order_by('pk')
     batch = batches.objects.get(name=request.user.batch.name)
     sliders = Carousel.objects.order_by('pk')
     num_visits = request.session.get('num_visits', 0)
     request.session['num_visits'] = num_visits + 1
-    context = { 'y':y, 'z':z, 'num_visits':num_visits, 'batch':batch, 'sliders':sliders, 'x':x,'a':a, 'b':b }
+    context = { 'schedules':schedules, 'attendance':attendance, 'num_visits':num_visits, 'batch':batch, 'sliders':sliders, 'results':results,'naca':naca, 'camp':camp }
     return render(request, template_name, context)
 @login_required
 @student_required
